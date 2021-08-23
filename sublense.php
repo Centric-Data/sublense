256161ee4d76443ce1be1f0d49650317578ccaca
gillmourtunhira <gillmourtunhira@gmail.com>
2021-08-23 14:05:52 +0200

    shortcode html function call

--- sublense.php
+++ sublense.php
@@ -41,8 +41,57 @@ define( 'SLF_PLUGIN_URL', plugin_dir_url( __FILE__ )
 class SubmitLenseForm{
 
       public function __construct(){
+          // enqueue scripts (js, css)
+          add_action( 'wp_enqueue_scripts', array( $this, 'slf_load_assets' ) );
+
+          // Call for the shortcode
+          add_shortcode( 'sub-lense', array( $this, 'slf_load_shortcode' ) );
+
 
       }
+
+      /**
+      * Add shortcode to the page
+      */
+      public function slf_load_assets(){
+          wp_enqueue_style( 'sublense-css' SLF_PLUGIN_URL . 'css/sublense.css', [], time(), 'all' );
+          wp_enqueue_script( 'sublense-js' SLF_PLUGIN_URL . 'js/sublense.js', [ 'jquery' ], time(), 1 );
+      }
+
+      /**
+      * Add shortcode to the page
+      */
+      public function slf_load_shortcode(){
+        ?>
+        <section>
+          <div class="submit__page lense-row">
+            <h3>Submission Form</h3>
+            <p>
+              <?php
+                echo esc_html__( 'Abstracts should be in English, with a maximum of 250 words. Please use single spacing and Times New Roman, font size 12. Abstracts of research papers should provide a brief description of research objectives, methodology, theory and summary of results and/or conclusions. Please do not include any charts, bibliographies or footnotes.', 'lands' );
+              ?>
+            </p>
+            <div class="submit__columns">
+                <form>
+                  <div class="submit__columns--form">
+                    <div class="submit__left">
+                      <input type="text" name="slf-name" id="slf-name">
+                      <input type="email" name="slf-email" id="slf-email">
+                      <input type="text" name="slf-business" id="slf-business">
+                      <textarea name="slf-abstract" id="slf-abstract" cols="30" rows="10"></textarea>
+                    </div>
+                    <div class="submit__right"></div>
+                  </div>
+                  <div class="submit__columns--button">
+                    <button id="submit_userform">Submit Form</button>
+                  </div>
+                </form>
+            </div>
+          </div>
+        </section>
+        <?php
+      }
+
 
 }
 
